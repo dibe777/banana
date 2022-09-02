862647f63af58474a72d96b984ce66eb1d52d623
Renato De la Rosa <srdelarosab@gmail.com>
2022-09-01 21:07:09 -0500

    Modificacion metodo Automerge

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ const GitApp = () => {
     console.log(`Branch HEADER`);
   };
 
-  const autoMerge = () => {
-    console.log(`AUTO MERGE 🤞`);
+  const autoMerge = (msg) => {
+    console.log(`AUTO MERGE 🤞: ${msg}`);
   };
 
   return { printHeader, printBody, printFooter, branchHeader, autoMerge };
