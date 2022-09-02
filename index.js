f5b61c467d4bf27dfcc370243e0d284fae64f134
mattcastillo <mattcastillo@outlook.com>
2022-09-01 21:38:50 -0500

    removiendo metodos

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
 const GitApp = () => {
-  const printHeader = (message) => {
-    console.log(`Header: ${message}`);
-  };
-
-  const printBody = (message) => {
-    console.log(`Body: ${message}`);
-  };
-
-  const printFooter = (message) => {
-    console.log(`Footer: ${message}`);
-  };
-
-  const branchHeader = () => {
-    console.log(`Branch HEADER`);
-  };
-
+  
   const autoMerge = (msg, title, author) => {
     console.log(`📢 ${author}: ${title}/${msg}`);
   };
 
-  return { printHeader, printBody, printFooter, branchHeader, autoMerge };
+  return { autoMerge };
 };
 
 export default GitApp;
